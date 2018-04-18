95b5e36b44f3bb2c91bc1e9bd69fcf8dd5503abd
mdkashf <mmashrafi@stuy.edu>
2018-04-18 14:03:54 -0400

    Cr8ed interface thing

---
+++ Deque.java
@@ -0,0 +1,36 @@
+public interface Deque<Card>
+{
+    public int size();
+
+    //public boolean isEmpty();
+
+    //public boolean offerFirst(Card c);
+
+    //public Card pollFirst();
+
+    //public Card peekFirst();
+
+    //public boolean offerlLast(Card c);
+
+    //public Card pollLast();
+
+    //public Card peekLast();
+
+    //public void addFirst(Card c);
+
+    //public Card removeFirst();
+
+    //public Card getFirst();
+
+    //public void addLast(Card c);
+
+    //public Card removeLast();
+
+    //public Card getLast();
+
+    
+
+    
+
+    
+}
